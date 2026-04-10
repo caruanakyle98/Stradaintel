31059e2a572260bc731488fb7885a89cd9d4f7d6
caruanakyle98 <caruanakyle@gmail.com>
2026-04-10 13:51:36 +0400

    feat: add Google Analytics (gtag.js) tracking
    - Add gtag.js script with measurement ID G-H9QT8NSR9D
    - Configure analytics to track page views and events
    - Both Google Tag Manager and Google Analytics now active

--- app/layout.js
+++ app/layout.js
@@ -30,8 +30,19 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning className={`${montserrat.variable} ${poppins.variable}`}>
       <head>
+        {/* Google Tag Manager */}
         <Script id="gtm" strategy="beforeInteractive" dangerouslySetInnerHTML={{
           __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KCBPD9JV');`
+        }} />
+        {/* Google Analytics (gtag.js) */}
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-H9QT8NSR9D" />
+        <Script id="gtag-config" strategy="afterInteractive" dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-H9QT8NSR9D');
+          `
         }} />
       </head>
       <body
